2787b58cfa2d99f014228322e3d9682f6c70939f
Njohanson5 <148806146+Njohanson5@users.noreply.github.com>
2024-04-10 15:06:29 -0400

    Add files via upload

---
+++ PyBank/Main.py
@@ -0,0 +1,87 @@
+
+#Import
+import os
+import csv
+
+#specify the path to collect data from
+pybank_csv = os.path.join('C:/Users/njohanson/OneDrive - Federal Signal Corporation/Desktop/NU-VIRT-DATA-PT-10-2023-U-LOLC/02-Homework/03-Python/Instructions/Starter_Code/PyBank/Resources/budget_data.csv')
+
+
+with open(pybank_csv) as csvfile:
+    #define csvreader variable, and split the columns at the commas in the CSV file
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    csv_list = list(csvreader)
+    
+# month list
+month_list = [row[0] for row in csv_list]
+
+#profit list turns the Profits/Losses column into integers we can work with later
+profit_list = [int(row[1]) for row in csv_list]
+
+# total months (calculated by using len to count the rows)
+total_months = len(month_list)
+
+# total profit
+total_profit = sum(profit_list)
+
+# list of profit changes within the range of months
+profit_change = [profit_list[i] - profit_list[i-1] for i in range(1, total_months)]
+
+month_change = [month_list[i] for i in range(1, total_months)]
+
+average_change = sum(profit_change)/(total_months-1)
+
+# find greatest increase in profits, and greatest decrease within the range. Define the date variables to correspond with the min/max profit rows
+greatest_increase = max(profit_change)
+for i in range(total_months-1):
+    if profit_change[i]==greatest_increase:
+        greatest_increase_date = month_change[i]
+        
+greatest_decrease = min(profit_change)
+for i in range(total_months-1):
+    if profit_change[i]==greatest_decrease:
+        greatest_decrease_date = month_change[i]
+        
+
+#print output, making sure to round average change to 2 decimal points 
+print("Financial Analysis")     
+print("----------------------------")       
+print(f"Total Months: "+ str(total_months))
+print(f"Total: $" + str(total_profit))
+print(f"Average Change: $ {round(average_change,2)}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_increase_date} (${greatest_decrease})")
+
+# Specify the file path to write new file called "pybank_analysis.txt" into
+output_path = os.path.join(os.path)
+# Open the file using "write" mode. Specify the variable to hold the contents as textfile
+with open(output_path, 'w') as textfile:
+    # Write the txt 
+    textfile.write("Financial Analysis\n")
+    textfile.write("----------------------------\n")
+    textfile.write(f"Total Months: {total_months}\n")
+    textfile.write(f"{'Total: $'} {total_profit:,}\n")
+    textfile.write(f"{'Average Change: '} ${round(average_change,2):,}\n")
+    textfile.write(f"{'Greatest Increase in Profits: '} {greatest_increase_date} (${greatest_increase})\n")
+    textfile.write(f"{'Greatest Increase in Profits: '} {greatest_increase_date} (${greatest_decrease})\n")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+
+
---
+++ Pypoll/main.py
@@ -0,0 +1,81 @@
+#dependencies
+import os
+import csv
+
+#specify the path to collect data from
+data = os.path.join('C:/Users/njohanson/OneDrive - Federal Signal Corporation/Desktop/NU-VIRT-DATA-PT-10-2023-U-LOLC/02-Homework/03-Python/Instructions/Starter_Code/PyPoll/Resources/election_data.csv')
+
+#read in the CSV file
+with open(data) as csvfile:
+
+    #define csvreader variable, and split the columns at the commas in the CSV file
+    reader = csv.reader(csvfile, delimiter=',')
+    #read header row, and move to next row
+    row = next(reader)
+    #make the csv file into a list we can pull data from   
+    list = list(reader)
+#count the number of rows after the header, to calculate the amt of votes. 
+votes = [row[2] for row in list]
+total_votes = (len(votes))
+
+#start list of tallies and names, so I can store info for each candidate's name and their vote tallies
+tallies = []
+names = []
+
+#for each name in the candidates column, if the name is unique from the one above it, add it to the list. Additionally,
+# record a tally. Tally count starts at 0
+for name in votes:
+    if name not in names:
+        names.append(name)
+        tallies.append(0)
+
+#count how many times each name is repeated, record the total for each candidate's name
+for vote in votes:
+     for i, name in enumerate(names):
+        if vote == name:
+             tallies[i] +=1 
+
+#identify which name has received the most votes
+for i, name in enumerate(names):
+
+    most = max(tallies)
+
+#if a candidate's name appears the most, they are the winner!
+for i, vote in enumerate(tallies):
+    if vote == most:
+        winner = names[i]
+
+#print output
+print(f"Election Results\n")
+print(f"-------------------------\n")
+print(f"Total Votes:  {total_votes}\n")
+print(f"-------------------------\n")
+
+for i, name in enumerate(names):
+    print(f"{name:<24}  {(tallies[i]/total_votes)*100:5,.2f}% ({tallies[i]:7,}) \n")             
+
+       
+print(f"-------------------------\n")
+print(f"Winner: {winner}\n")
+print(f"-------------------------\n")
+
+# Specify the file to write to
+output_path = os.path.join(os.path)
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as textfile:
+
+    # Initialize csv.writer
+    
+    # Write the first row (column headers)
+    textfile.write(f"Election Results\n")
+    textfile.write(f"-------------------------\n")
+    textfile.write(f"Total Votes:  {total_votes}\n")
+    textfile.write(f"-------------------------\n")
+
+    for i, name in enumerate(names):
+        textfile.write(f"{name:<24}  {(tallies[i]/total_votes)*100:5,.2f}% ({tallies[i]:7,})\n")             
+
+       
+    textfile.write(f"-------------------------\n")
+    textfile.write(f"Winner: {winner}\n")                       
+    textfile.write(f"-------------------------\n")
